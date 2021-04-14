69e7cf6dc7a575b0bfeda1f233aad1dccf5fd844
Paulo Lima <prmlimajr@hotmail.com>
2021-04-14 16:51:18 -0300

    Created detail page

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,18 +1,58 @@
 import React, {useState, useEffect} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Loader from '../../components/Loader';
-import {SafeAreaView, Alert, Text} from 'react-native';
-import {useRoute} from '@react-navigation/native';
+import * as Progress from 'react-native-progress';
+import {SafeAreaView, Alert, Text, View} from 'react-native';
+import {useRoute, useNavigation} from '@react-navigation/native';
 import api from '../../services/api';
-import {Container, PokemonName, PokemonImage} from './styles';
-import {getPokemonImage} from '../../utils';
-import {FlatList} from 'react-native-gesture-handler';
+import {
+  Container,
+  Header,
+  PokemonName,
+  PokemonImage,
+  PokemonID,
+  PokemonData,
+  PokemonStatus,
+  PokemonIngame,
+  PokemonImagesView,
+  PokemonIngameView,
+  Types,
+  PokemonType,
+  StatView,
+  StatName,
+} from './styles';
+import {getPokemonImage, padToThree} from '../../utils';
+import {
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
 
 interface Pokemon {
+  id: number;
   name: string;
   url: string;
   moves: Array<MoveProps>;
+  stats: Array<StatsProps>;
+  sprites: {
+    back_default: string;
+    front_default: string;
+  };
+  types: Array<TypesProps>;
 }
 
+interface TypesProps {
+  type: {
+    name: string;
+  };
+}
+
+interface StatsProps {
+  base_state: number;
+  stat: {
+    name: string;
+  };
+}
 interface MoveProps {
   name: string;
 }
@@ -25,6 +65,7 @@ interface Params {
 export default function Detail({name, url}: Pokemon) {
   const [pokemon, setPokemon] = useState({} as Pokemon);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const navigation = useNavigation();
 
   const route = useRoute();
   const routeParams = route.params as Params;
@@ -46,21 +87,79 @@ export default function Detail({name, url}: Pokemon) {
     }
   }
 
-  return (
-    <SafeAreaView style={{flex: 1, backgroundColor: '#FFF'}}>
-      {isLoading && <Loader />}
-      <Container>
-        <PokemonName>{routeParams.name}</PokemonName>
-        <PokemonImage source={getPokemonImage(routeParams.url)} />
+  console.log(pokemon);
 
-        <FlatList
-          data={pokemon.moves}
-          keyExtractor={move => move.name}
-          renderItem={({item}) => {
-            return <Text>{item.name}</Text>;
-          }}
-        />
-      </Container>
-    </SafeAreaView>
+  return (
+    pokemon.hasOwnProperty('id') && (
+      <SafeAreaView style={{flex: 1, backgroundColor: '#FFF'}}>
+        {isLoading && <Loader />}
+        <ScrollView>
+          <Container>
+            <Header>
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Icon name="arrow-left" size={24} color="#000" />
+              </TouchableOpacity>
+              <PokemonName>{routeParams.name}</PokemonName>
+            </Header>
+            <PokemonID>#{padToThree(pokemon.id)}</PokemonID>
+
+            <PokemonImagesView>
+              <PokemonImage source={getPokemonImage(routeParams.url)} />
+
+              <PokemonIngameView>
+                {pokemon.sprites ? (
+                  <PokemonIngame
+                    source={{uri: pokemon.sprites.front_default}}
+                  />
+                ) : null}
+                {pokemon.sprites ? (
+                  <PokemonIngame source={{uri: pokemon.sprites.back_default}} />
+                ) : null}
+              </PokemonIngameView>
+            </PokemonImagesView>
+
+            <PokemonData>
+              <Types>
+                {pokemon.types.map(t => {
+                  return <PokemonType>{t.type.name.toUpperCase()}</PokemonType>;
+                })}
+              </Types>
+              <PokemonStatus>
+                <FlatList
+                  data={pokemon.stats}
+                  keyExtractor={stat => stat.stat.name}
+                  renderItem={({item}) => {
+                    return (
+                      <StatView>
+                        <StatName>{item.stat.name.toUpperCase()}:</StatName>
+                        <View>
+                          <Progress.Bar
+                            progress={item.base_stat / 100}
+                            width={200}
+                            animated
+                            height={20}
+                            borderRadius={10}
+                            color="#3fca95"
+                          />
+                        </View>
+                      </StatView>
+                    );
+                  }}
+                />
+              </PokemonStatus>
+            </PokemonData>
+
+            {/* <MoveList>Golpes:</MoveList>
+          <FlatList
+            data={pokemon.moves}
+            keyExtractor={move => move.name}
+            renderItem={({item}) => {
+              return <Text>{item.move.name}</Text>;
+            }}
+          /> */}
+          </Container>
+        </ScrollView>
+      </SafeAreaView>
+    )
   );
 }
--- src/pages/Detail/styles.ts
+++ src/pages/Detail/styles.ts
@@ -1,10 +1,83 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 18px;
+  background-color: #fff;
+`;
 
-export const PokemonName = styled.Text``;
+export const Header = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const PokemonName = styled.Text`
+  font-weight: bold;
+  font-size: 30px;
+  margin-left: 10px;
+`;
 
 export const PokemonImage = styled.Image`
   width: 200px;
   height: 200px;
+`;
+
+export const MoveList = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const PokemonID = styled.Text``;
+
+export const PokemonData = styled.View`
+  margin: 20px 0;
+`;
+
+export const PokemonStatus = styled.View`
+  flex-direction: row;
+`;
+
+export const Stat = styled.View`
+  font-weight: bold;
+`;
+
+export const PokemonIngame = styled.Image`
+  width: 140px;
+  height: 140px;
+`;
+
+export const PokemonImagesView = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const PokemonIngameView = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StatView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const StatName = styled.Text`
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const Types = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const PokemonType = styled.Text`
+  margin-right: 10px;
+  padding: 5px 10px;
+  border-radius: 20px;
+  border: 2px solid #f0f0f0;
+  color: #c4c4c4;
 `;
